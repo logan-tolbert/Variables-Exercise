764fabd73bf749f8e9164f5463e713330ec6c4d2
logan-tolbert <logo88205+git@gmail.com>
2024-07-26 17:37:37 -0500

    complete exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //heading
+            Console.WriteLine("\t- Variables Exercise -\n");
+
+
+            //step 1: Declare and initialize variable
+            string name = "Logan";
+            int meaningOfLife = 42;
+            char grade = 'A';
+            bool enabled = false;
+            double myDoubleNum = 14.25;
+            decimal myDecimalNum1 = 42.3545654347m;
+            decimal myDeciamlNum2 = 33.333897m;
+
+            //step 2: interpolate variables to string
+            Console.WriteLine("Type\t\tUsage");
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine($"string\t| My name is {name}\n");
+            Console.WriteLine($"int\t| The meaning of life is {meaningOfLife}\n");
+            Console.WriteLine($"char\t| I got an \'{grade}\' in Computer Science!\n");
+            Console.WriteLine($"bool\t| Enabled = {enabled}\n");
+            Console.WriteLine($"double\t| Starting Pay: ${myDoubleNum}\n");
+            Console.WriteLine($"decimal\t| {myDecimalNum1} * {myDeciamlNum2} = {myDecimalNum1 * myDeciamlNum2}\n");
+            
         }
     }
 }
--- VariablesExercise/obj/Release/net6.0/VariablesExercise.AssemblyInfo.cs
+++ VariablesExercise/obj/Release/net6.0/VariablesExercise.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("VariablesExercise")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b34bde4a43c9b054b760a9b9d11923d55adbe7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("VariablesExercise")]
